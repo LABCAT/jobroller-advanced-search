e8dc4a0bc77708dc0f399f405b98fe2e72803c09
LABCAT <shane.watters.nz@gmail.com>
2019-08-06 10:15:01 +1200

    Extracting CSS into separate file so job tile styles can be applied to job preview form.

--- _dev/react-app/webpack.config.js
+++ _dev/react-app/webpack.config.js
@@ -1,21 +1,61 @@
 const path = require("path")
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 const glob = require("glob")
+
+// include the js minification plugin
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+
+// include the css extraction and minification plugins
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+const devMode = (process.argv.slice().pop() !== 'production');
 
 module.exports = {
     entry: {
         "bundle.js": glob.sync("build/static/?(js|css)/*.?(js|css)").map(f => path.resolve(__dirname, f)),
     },
     output: {
-        filename: "../../../assets/js/jobs-archive.min.js",
+        path: path.resolve(__dirname),
+        filename: "../../assets/js/jobs-archive.min.js",
     },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
+                test: /\.css?$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
             },
+            {
+                test: /\.(sass|scss)$/,
+                use: [
+                        {
+                            loader: devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                ]
+            }
         ],
     },
-    plugins: [new UglifyJsPlugin()],
+    plugins: [
+        new UglifyJsPlugin(),
+        // extract css into dedicated file
+        new MiniCssExtractPlugin(
+            {
+                filename: "../../assets/css/jobs-archive.min.css",
+            }
+        ),
+    ],
 }
--- includes/class-jas-frontend-scripts.php
+++ includes/class-jas-frontend-scripts.php
@@ -21,14 +21,26 @@ class JAS_Frontend_Scripts {
      * Hook in methods.
      */
     public static function init() {
-        add_action( 'wp_enqueue_scripts', [ __CLASS__, 'enqueue_scripts' ] );
+        add_action( 'wp_enqueue_scripts', [ __CLASS__, 'enqueue_styles' ], 999 );
+        add_action( 'wp_enqueue_scripts', [ __CLASS__, 'enqueue_scripts' ], 999 );
     }
 
+    public static function enqueue_styles(){
+        $css = 'assets/css/jobs-archive.min.css';
+        $cache_bust = '?v='.filemtime( JAS_ABSPATH . $css);
+        $style_location = JAS_URL  . $css . $cache_bust;
+
+        wp_enqueue_style(
+            'react-jobs-archive',
+            $style_location,
+            [],
+            NULL
+        );
+    }
 
     public static function enqueue_scripts() {
         $js = 'assets/js/jobs-archive.min.js';
         $cache_bust = '?v='.filemtime( JAS_ABSPATH . $js);
-
         $script_location = JAS_URL  . $js . $cache_bust;
 
         wp_enqueue_script(
