30b55a0acd019351f174e35874de3a083d9cec76
LABCAT <shane.watters.nz@gmail.com>
2019-08-06 12:49:53 +1200

    Adding 'upgrade browser' message for ie8 and below.

--- templates/archive-job_listing.php
+++ templates/archive-job_listing.php
@@ -5,8 +5,21 @@
 ?>
 
     <?php get_header( 'search' ); ?>
+   
+    <!--[if lte IE 8]> 
+        <div class="notice error" style="clear:both;">
+            <div>
+                <i class="icon dashicons-before"></i>
+                <p>
+                    The Do Good Jobs website doesn't support Internet Explorer version 8 or earlier, and your browser may not be able to display jobs on this website.
+                    For a faster, safer browsing experience, please <a href="https://updatemybrowser.org/" target="_blank">upgrade your web browser</a> today."       
+                </p>                   
+            </div>
+        </div>
+    <![endif]-->
 
     <div id="job-listings"></div>
+    
     
     <div class="clear"></div>
 
